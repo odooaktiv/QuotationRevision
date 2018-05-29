e771ab7cc26c1d78f364b68f3a97b864dd1a8c3c
Virali Aktiv <mgajjar@nishutatechnolabs.com>
2018-05-29 11:40:17 +0530

    [INIT] Initial commit for module quotation revision in master

---
+++ quotation_revision/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Quotation Revision",
+
+    'summary': """
+        Quotation Revised History""",
+
+    'description': """
+        We can manage Quotation Revised History in sale order.
+    """,
+
+    'author': "Aktiv software ",
+    'website': "http://www.aktivsoftware.com/",
+
+    # Categories can be used to filter modules in modules listing
+    'category': 'Sales',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['sale_management'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/sale_order_views.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+    ],
+    
+    'images':  ['static/description/banner.jpg'],
+    'application': True,
+}
